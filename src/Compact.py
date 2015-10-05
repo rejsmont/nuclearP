5c311c1004f834d5c9e1912e4531e5165ee8fb36
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2015-10-05 12:24:07 +0200

    Scoring implemented

--- src/Compact.py
+++ src/Compact.py
@@ -39,7 +39,6 @@ for index, line in enumerate(reader):
         voxel['newcoords'] = numpy.full((1, 2), -1)
         voxel['volume'] = line[4]
         voxel['values'] = numpy.array([[line[5], line[7]]])
-        voxel['visited'] = False
         voxels.append(voxel)
 
 ncount = len(voxels)
@@ -54,7 +53,6 @@ tree = scipy.spatial.cKDTree(matrix)
 distances, neighbors = tree.query(matrix, k=maxneigh)
 
 sys.setrecursionlimit(100000)
-numpy.set_printoptions(threshold='1024')
 
 class NuclearCluster():
     
@@ -68,10 +66,9 @@ class NuclearCluster():
         self.width = width
         self.height = height        
         self.target = numpy.full( \
-            (int(self.width), int(self.height)), -1, dtype=int)
+            (self.width, self.height), -1, dtype=int)
         self.coordinates = numpy.full((self.n_items, 2), -1, dtype=int)
         self.visited = numpy.zeros((self.n_items, 1), dtype=bool)
-        self.score = 0
 
 
     ### Calculate relative position of the neighbor ###
@@ -87,11 +84,12 @@ class NuclearCluster():
     ### Fill neighborhood recursively ###
     def nfill(self, x, y, item):
         self.target[x, y] = item
-        self.coordinates[item] = numpy.array([x, y])
+        self.coordinates[item, 0] = x
+        self.coordinates[item, 1] = y
         self.visited[item] = True
-        
+                
         for n_no in range(1, self.max_neigh):
-            neigh = self.neighbors[item][n_no]
+            neigh = self.neighbors[item, n_no]
             if neigh >= self.n_items:
                 break
             if self.visited[neigh]:
@@ -100,7 +98,10 @@ class NuclearCluster():
             C = self.nposition(item, neigh)
             Cx = C[0]
             Cy = C[1]
-
+            
+            if Cx >= self.width or Cy > self.height:
+                continue
+            
             if self.target[x + Cx, y + Cy] == -1:
                 self.nfill(x + Cx, y + Cy, neigh)
 
@@ -110,8 +111,8 @@ class NuclearCluster():
         list = numpy.arange(self.n_items)
         placed = numpy.empty(0, dtype=int)
         for item in range(0, self.n_items):
-            if self.coordinates[item][0] != -1 and \
-                self.coordinates[item][1] != -1:
+            if self.coordinates[item, 0] != -1 and \
+                self.coordinates[item, 1] != -1:
                 placed = numpy.append(placed, item)
         missing = numpy.delete(list, placed)
         numpy.random.shuffle(missing)
@@ -131,6 +132,13 @@ class NuclearCluster():
             if T[1] != 0:
                 self.target[W[0], :] = \
                     self.insert(item, self.target[W[0], :], W[1], T[1])
+        
+        for x in range(0, self.width):
+            for y in range(0, self.height):
+                item = self.target[x, y]
+                if item != -1:
+                    self.coordinates[item, 0] = x
+                    self.coordinates[item, 1] = y
     
     
     ### Insert value into a specified position in vector
@@ -170,9 +178,9 @@ class NuclearCluster():
     ### Find a place for an item in the matrix ###
     def anchor(self, item):
         for n_no in range(1, maxneigh):
-            neigh = neighbors[item][n_no]
-            nx = self.coordinates[neigh][0]
-            ny = self.coordinates[neigh][1]
+            neigh = neighbors[item, n_no]
+            nx = self.coordinates[neigh, 0]
+            ny = self.coordinates[neigh, 1]
             if nx != -1 and ny != -1:
                 break;
         
@@ -188,6 +196,8 @@ class NuclearCluster():
     
     ### What is the best axis to displace when placing the item ###
     def best_axis(self, x, y):
+        nx = px = x
+        ny = py = y
         for nx in range(x, -1, -1):
             if self.target[nx, y] == -1:
                 break
@@ -210,8 +220,53 @@ class NuclearCluster():
             2: numpy.array([0, -V[2]]),
             3: numpy.array([0, V[3]])
         }.get(axis, numpy.array([0, 0]))
+    
+    
+    ### Compact the target matrix
+    def compact(self):
+        minx = numpy.amin(self.coordinates[:, 0])
+        miny = numpy.amin(self.coordinates[:, 1])
+        maxx = numpy.amax(self.coordinates[:, 0])
+        maxy = numpy.amax(self.coordinates[:, 1])
+        rwidth = maxx - minx + 1
+        rheight = maxy - miny + 1
+        rmatrix = numpy.full((rwidth, rheight), -1, dtype=int)
+
+        for rx in range(minx, maxx+1):
+            for ry in range(miny, maxy+1):
+                rmatrix[rx - minx, ry - miny] = self.target[rx, ry]
+                
+        return rmatrix
+    
+    
+    ### Score target matrix
+    def score(self):
+        score = 0
+        for item in range(0, self.n_items):
+            ix = self.coordinates[item, 0]
+            iy = self.coordinates[item, 1]
+            if ix != -1 and iy != -1:
+                for x in range(ix - 1, ix + 2):
+                    for y in range(iy - 1, iy + 2):
+                        if x == 1 and y == 1:
+                            continue
+                        neigh = self.target[x, y]
+                        if neigh == -1:
+                            score = score + 50
+                            continue
+                        npos = -1
+                        for n in range (0, self.max_neigh):
+                            if (neigh == self.neighbors[item, n]):
+                                score = score + self.distances[item, n]
+                                npos = n
+                                break
+                        if npos == -1:
+                            score = score + 100
         
-        
+        return score
+                                
+    
+    
     ### Initiate neighborhood fill sequence ###
     def fill(self):
         seed = numpy.random.randint(0, self.n_items)
@@ -221,81 +276,9 @@ class NuclearCluster():
 
 cluster = NuclearCluster(matrix, distances, neighbors, width, height)
 cluster.fill()
+score = cluster.score()
+result = cluster.compact()
 
-
-
-#for iterations in range(0, 1):
-#    
-#    list = numpy.arange(ncount)
-#    voxlist = copy.deepcopy(voxels)
-#    target = numpy.full((int(width), int(height)), -1, dtype=int)
-#    
-#    ### Get initial estimate matrix by filling the neighborhood with nearest neighbors
-#    sys.setrecursionlimit(100000)
-#    seed = numpy.random.randint(0, ncount)
-#    fill_neighborhood(int(width/2), int(height/2), list[seed], voxlist, target, matrix)
-#
-#    ### Check which items have been already placed
-#    placed = numpy.empty(0, dtype=int)
-#        
-#    for x in range(0, width):
-#        for y in range(0, height):
-#            nucleus = target[x, y]
-#            if nucleus != -1:
-#                placed = numpy.append(placed, nucleus)
-#                
-#    missing = numpy.delete(list, placed)
-#    numpy.random.shuffle(missing)
-#    
-#    for i in range(0, len(missing)):
-#        item = missing[i]
-#        for neighno in range(1, maxneigh):
-#            neigh = neighbors[item][neighno]
-#            nx = voxlist[neigh]['newcoords'][0][0]
-#            ny = voxlist[neigh]['newcoords'][0][1]
-#            if nx != -1 and ny != -1:
-#                break;
-#        
-#        C = neigh_position(item, neigh, matrix)
-#        Cx = -C[0]
-#        Cy = -C[1]
-#        
-#        if nx != -1 and ny != -1:
-#            x = nx + Cx
-#            y = ny + Cy
-#        else:
-#            x = -1
-#            y = -1
-#        
-#        print("%i: %i %i of %i: %i %i" % (item, Cx, Cy, neigh, x, y))
-#        
-#    
-#    
-#    minx = width
-#    miny = height
-#    maxx = 0
-#    maxy = 0
-#
-#    for x in range(0, 1024):
-#        for y in range(0, 1024):
-#            if target[x, y] != -1:
-#                if x < minx:
-#                    minx = x
-#                if x > maxx:
-#                    maxx = x
-#                if y < miny:
-#                    miny = y
-#                if y > maxy:
-#                    maxy = y
-#
-#    rwidth = maxx - minx + 1
-#    rheight = maxy - miny + 1
-#
-#    rmatrix = numpy.full((rwidth, rheight), -1, dtype=int)
-#
-#
-#    for rx in range(minx, maxx+1):
-#        for ry in range(miny, maxy+1):
-#            rmatrix[rx - minx, ry - miny] = target[rx, ry]
-#
-#    print("The matrix contains %i out of %i nuclei" % (nplaced, ncount))
+print("Nuclei: %i", cluster.n_items)
+print("Score: %f", score)
+print(result.shape)
